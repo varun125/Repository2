dd6a7f85257c61af84e547b4af830124137507ce
varun125 <varunkumar0387@gmail.com>
2019-09-23 23:18:40 +0530

    Added second line

--- Re-Project1/src/pack1/TestClass.java
+++ Re-Project1/src/pack1/TestClass.java
@@ -3,7 +3,7 @@ package pack1;
 public class TestClass {
 
 	public static void main(String[] args) {
-		System.out.println("Hello");
+		System.out.println("Hi");
 		// TODO Auto-generated method stub
 
 	}
