b37e297fddecefe8e1959f9a739bee9fef733562
varun125 <55709297+varun125@users.noreply.github.com>
2019-09-24 22:49:35 +0530

    Update TestClass.java
    Added a method

--- Re-Project1/src/pack1/TestClass.java
+++ Re-Project1/src/pack1/TestClass.java
@@ -1,7 +1,12 @@
 package pack1;
 
 public class TestClass {
-
+public void add()
+{
+	int a=10,b=10,c=0;
+	c=a+b;
+	System.out.println(c);
+}
 	public static void main(String[] args) {
 		System.out.println("Hi");
 		System.out.println("Hello");
